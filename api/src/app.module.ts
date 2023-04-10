9e94c483675c9e2c834206bb2d8ae39ac7dadd54
David ASLI <adn.asli.dev@gmail.com>
2023-04-10 19:21:08 +0200

    Basic server gateway in order to use websocket

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaService } from "./prisma/prisma.service";
+import { ServerGateway } from "./server/server.gateway";
 import { PrismaModule } from "./prisma/prisma.module";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
@@ -17,6 +18,6 @@ import { ConfigModule } from "@nestjs/config";
 		AuthModule,
 	],
 	controllers: [AppController],
-	providers: [AppService, PrismaService],
+	providers: [AppService, PrismaService, ServerGateway],
 })
 export class AppModule {}
