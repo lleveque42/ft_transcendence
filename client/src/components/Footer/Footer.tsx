9e3392c947a501329abe0a78467110a051089185
arudy <arudy@student.42.fr>
2023-04-14 16:29:09 +0200

    Header: done for now

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
 // import styles from "./Footer.module.scss";
 
 export default function Footer() {
+	const navigate = useNavigate();
+	const { logout } = useAuth();
+
+	const signout = () => {
+		logout();
+		navigate("/login");
+	};
+
+	async function handleClickDeleteAllDatabase() {
+		// To del
+		try {
+			const res = await fetch("http://localhost:3000/user/temporary_dropdb", {
+				method: "DELETE",
+				credentials: "include",
+			});
+			if (res.status === 410) {
+				signout();
+			}
+		} catch (e) {
+			console.error("Error remove users DB: ", e);
+		}
+	}
+
 	return (
-		<footer>
+		<footer className="d-flex justify-content-space-between">
 			<p>Copyright © 2023 ft_transcendence</p>
+			<button
+				className="btn-danger mr-5"
+				onClick={handleClickDeleteAllDatabase}
+			>
+				Empty users db
+			</button>
 		</footer>
 	);
 }
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
 import styles from "./Header.module.scss";
-import photo from "../../assets/images/medium_arudy.jpg";
+import photo from "../../assets/images/punk.png";
 import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
@@ -15,28 +15,14 @@ export default function Header() {
 		navigate("/login");
 	};
 
-	async function handleClickDeleteAllDatabase() {
-		// To del
-
-		try {
-			const res = await fetch("http://localhost:3000/user/temporary_dropdb", {
-				method: "DELETE",
-				credentials: "include",
-			});
-			if (res.status === 410) {
-				signout();
-			}
-		} catch (e) {
-			console.error("Error remove users DB: ", e);
-		}
-	}
-
 	useEffect(() => {
-		const handle = (e: Event) => {
-			// if (!menuRef.current.contains(e.target)) setOpenMenu(false);
+		const handle = (e: any) => {
+			if (menuRef.current && !menuRef.current.contains(e.target)) {
+				setOpenMenu(false);
+			}
 		};
 		document.addEventListener("mousedown", handle);
-		return () => document.addEventListener("mousedown", handle);
+		return () => document.removeEventListener("mousedown", handle);
 	});
 
 	return (
@@ -44,7 +30,14 @@ export default function Header() {
 			<div
 				className={`${styles.headerContainer} d-flex align-items justify-content-space-between`}
 			>
-				<h2 className={`${styles.title} pl-10`}>FT_TRANSCENDENCE</h2>
+				<h2
+					className={`${styles.title} pl-10`}
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					FT_TRANSCENDENCE
+				</h2>
 
 				<div className={`${styles.menuContainer}`} ref={menuRef}>
 					<div
@@ -53,42 +46,26 @@ export default function Header() {
 					>
 						<img src={photo} />
 					</div>
-
-					<div className={`${styles.dropDown}`}>
-						{/* ${open? 'active' : 'inactive'} */}
+					<div
+						className={`${styles.dropdownMenu} ${
+							openMenu ? styles.active : styles.inactive
+						}`}
+					>
+						<h3>Hey Username</h3>
 						<ul>
-							<li className={`${styles.menuItem}`}>
-								<button
-									className="btn btn-primary mr-5"
-									onClick={() => {
-										navigate("/editprofile");
-									}}
-								>
-									Edit Profile
-								</button>
+							<li
+								className={`${styles.dropdownItem}`}
+								onClick={() => {
+									navigate("/editprofile");
+								}}
+							>
+								<a>My Profile</a>
 							</li>
-							<li className={`${styles.menuItem}`}>
-								<button className="btn-danger mr-5" onClick={signout}>
-									Logout
-								</button>
+							<li className={`${styles.dropdownItem}`} onClick={signout}>
+								<a>Logout</a>
 							</li>
 						</ul>
 					</div>
-
-					{/* <button
-						className="btn btn-primary mr-5"
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						Homepage
-					</button>
-					<button
-						className="btn-danger mr-5"
-						onClick={handleClickDeleteAllDatabase}
-					>
-						Empty users db
-					</button> */}
 				</div>
 			</div>
 		</header>
