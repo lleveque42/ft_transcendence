cd3f0a4d047f33cf48796a124dea07190acf37c1
lleveque42 <louis.leveque@outlook.com>
2023-05-03 14:48:53 +0200

    app and game gateway update: class OnlineUsers added

--- api/src/app.gateway.ts
+++ api/src/app.gateway.ts
@@ -12,11 +12,7 @@ import {
 import { Namespace, Socket } from "socket.io";
 import { UserService } from "./user/user.service";
 import { User } from "@prisma/client";
-
-interface Pair {
-	client: Socket;
-	user: User;
-}
+import { OnlineUsers } from "./classes/OnlineUsers";
 
 @WebSocketGateway(8001, { cors: "*" })
 export class AppGateway
@@ -25,8 +21,7 @@ export class AppGateway
 	private readonly logger = new Logger(AppGateway.name);
 	@WebSocketServer()
 	io: Namespace;
-	socketToUser: Map<string, Pair> = new Map<string, Pair>();
-	userToSocket: Map<number, Pair> = new Map<number, Pair>();
+	users: OnlineUsers = new OnlineUsers();
 
 	constructor(private userService: UserService) {}
 
@@ -35,41 +30,25 @@ export class AppGateway
 	}
 
 	handleDisconnect(client: Socket) {
-		const pair = this.socketToUser.get(client.id);
-		if (!pair) return;
-		this.logger.log(
-			`WS Client ${client.id} (${pair.user.userName}) disconnected !`,
-		);
-		this.userToSocket.delete(pair.user.id);
-		this.socketToUser.delete(client.id);
-		this.logger.log(`${this.socketToUser.size} user(s) connected !`);
+		const user: User = this.users.getUserByClientId(client.id);
+		if (!user) return;
+		this.logger.log(`WS Client ${client.id} (${user.userName}) disconnected !`);
+		this.users.removeClientId(client.id);
+		this.logger.log(`${this.users.size} user(s) connected !`);
 	}
 
 	async handleConnection(@ConnectedSocket() client: Socket) {
 		const user = await this.userService.getUserByEmail(
 			`${client.handshake.query.email}`,
 		);
-
-		// si un user est déjà connecté sur une autre page, on le reco sur cette page
-		// comme si de rien était mais on le comptabilise pas comme un nouveau user
-		if (this.userToSocket.has(user.id))
-			return;
-
+		if (this.users.hasByUserId(user.id))
+			this.users.addClientToUserId(user.id, client);
+		else this.users.addNewUser(user, client);
 		this.logger.log(`WS Client ${client.id} (${user.userName}) connected !`);
-		this.socketToUser.set(client.id, {
-			client,
-			user,
-		});
-		this.userToSocket.set(user.id, {
-			client,
-			user,
-		});
-		this.logger.log(`${this.socketToUser.size} user(s) connected !`);
+		this.logger.log(`${this.users.size} user(s) connected !`);
+	}
 
-		// // afficher tous les users
-		// this.socketToUser.forEach((value) => {
-		// 	console.log("new user");
-		// 	console.log(value.user);
-		// });
+	@SubscribeMessage("showUsers")
+	showUsers(@ConnectedSocket() client: Socket) {
 	}
 }
